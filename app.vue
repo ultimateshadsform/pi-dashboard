558f188008e317ec6b69ffef71952de05f4fae57
Alexander <alexxycarlsson@gmail.com>
2024-10-09 09:41:35 +0200

    Install tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: 'Pi Dashboard',
+})
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello World</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['@/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+        },
+      },
+    },
+  },
+});
